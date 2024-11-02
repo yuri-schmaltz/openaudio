f0f2d01ed07c1818a7259216465b6b2b61a5b4f6
cocktailpeanut <cocktailpeanuts@proton.me>
2024-11-02 14:14:56 -0400

    update

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/fishaudio/fish-speech app",
+          "git clone https://github.com/peanutcocktail/fish-speech app",
         ]
       }
     },
